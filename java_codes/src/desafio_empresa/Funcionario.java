87fae985e54df7352202f635df5a9b98f6779779
Pedro Gouveia <pedrohenriquecombr14@gmail.com>
2026-03-19 23:01:13 -0300

    organizando desafio empresa

---
+++ java_codes/src/desafio_empresa/Funcionario.java
@@ -0,0 +1,31 @@
+package java_codes.src.desafio_empresa;
+public class Funcionario {
+    private String nome;
+    private double salarioBase;
+
+    public Funcionario(String nome, double salarioBase){
+        this.nome = nome;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public double getSalario(){
+        return salarioBase;
+    }
+
+    public void setSalario(double salarioBase){
+        this.salarioBase = salarioBase;
+    }
+
+    public void calcularSalario(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Salário Base: " + salarioBase);
+    }
+}
---
+++ java_codes/src/desafio_empresa/Gerente.java
@@ -0,0 +1,24 @@
+package java_codes.src.desafio_empresa;
+public class Gerente extends Funcionario{
+    private double bonus;
+
+    public Gerente(String nome, double salario, double bonus){
+        super(nome, salario);
+        this.bonus = bonus;
+    }
+
+    public double getBonus(){
+        return bonus;
+    }
+
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+    }
+
+    @Override
+    public void calcularSalario(){
+        super.calcularSalario();
+        double salarioFinal = getSalario() + bonus;
+        System.out.println("Salário Final: " + salarioFinal);
+    }
+}
